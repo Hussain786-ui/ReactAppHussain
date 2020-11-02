8b7dca1086a415a272b9079ad381e0fd96041397
Hussain786-ui <73413690+Hussain786-ui@users.noreply.github.com>
2020-11-02 14:38:41 +0530

    Updates

---
+++ precache-manifest.d4cfea1a270e541de1a521140e3bfb8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28e753aecea799e423f0d8832734d74",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cd36b97c859456e18dce",
+    "url": "/my-app/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a5d54e489de0593709ba",
+    "url": "/my-app/static/css/main.71741b2e.chunk.css"
+  },
+  {
+    "revision": "cd36b97c859456e18dce",
+    "url": "/my-app/static/js/2.c7d776fd.chunk.js"
+  },
+  {
+    "revision": "ed6b993532326238a25c68b99337d08d",
+    "url": "/my-app/static/js/2.c7d776fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d54e489de0593709ba",
+    "url": "/my-app/static/js/main.64c90de2.chunk.js"
+  },
+  {
+    "revision": "f899f149cee7fc61157c",
+    "url": "/my-app/static/js/runtime-main.e8600da2.js"
+  }
+]);
